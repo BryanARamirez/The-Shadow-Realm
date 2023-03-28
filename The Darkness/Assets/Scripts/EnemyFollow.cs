ce75c55f0c0dfa40c8195b4187ad60c571f3b813
Domags99 <Djaguilar@mail.csuchico.edu>
2023-03-27 21:12:54 -0700

    EnemyFollow
    Fixed the enemy follow script so it only follows if you get in a certain range

--- The Darkness/Assets/Scripts/EnemyFollow.cs
+++ The Darkness/Assets/Scripts/EnemyFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
-    public NavMeshAgent enemy;
+    /*public NavMeshAgent enemy;
     public Transform Player;
     public float speed = 25;
    
@@ -15,5 +15,23 @@ public class EnemyFollow : MonoBehaviour
     {
         enemy.SetDestination(Player.position);
 
+    }
+    */
+    public NavMeshAgent enemy;
+    public Transform player;    // reference to the player's Transform component
+    public float followRange = 10f;    // range at which the enemy will start following the player
+    public float speed = 5f;    // speed at which the enemy will move towards the player
+
+    private void Update()
+    {
+        // check the distance between the player and the enemy
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // if the player is within range, move towards the player
+        if (distanceToPlayer <= followRange)
+        {
+            transform.LookAt(player.position);
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
     }
 }
