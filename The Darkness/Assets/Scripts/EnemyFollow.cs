29e3bd094199042354e13c7b67a8b66b3b41cdb0
Domags99 <Djaguilar@mail.csuchico.edu>
2023-03-29 17:59:07 -0700

    EnemyFollow
    Took out some code that wasn't being used to clear up some space

--- The Darkness/Assets/Scripts/EnemyFollow.cs
+++ The Darkness/Assets/Scripts/EnemyFollow.cs
@@ -5,28 +5,9 @@ using UnityEngine.AI;
 
 public class EnemyFollow : MonoBehaviour
 {
-    /*public NavMeshAgent enemy;
-    public Transform Player;
-    public float speed = 25;
-    [SerializeField] private Movement playerSneaking;
-   
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerSneaking.sneak == false)
-        {
-            gameObject.GetComponent<NavMeshAgent>().isStopped = false;
-            enemy.SetDestination(Player.position);
-        }
-        if (playerSneaking.sneak == true)
-        {
-            gameObject.GetComponent<NavMeshAgent>().isStopped = true;
-        }
-    }
-    */
+    
     public NavMeshAgent enemy;
-    public Transform player;    // reference to the player's Transform component
+    public Transform player;    
     public float followRange = 10f;    // range at which the enemy will start following the player
     public float speed = 1f;    // speed at which the enemy will move towards the player
     [SerializeField] private Movement playerSneaking; //To get the sneaking bool from the Movement script to stop the enemy from chasing when sneakings
