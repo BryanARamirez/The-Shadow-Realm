7a8b6ca1d94c8e0445074114028095a198afb0df
Domags99 <Djaguilar@mail.csuchico.edu>
2023-04-11 22:49:59 -0700

    jumpscare audio
    added some jumpscare audio triggers when the player collides with a trigger and it turns itself off when it is activated once.

---
+++ The Darkness/Assets/Scripts/JumpScareAudio.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScareAudio : MonoBehaviour
+{
+    
+    public AudioSource audioSource;
+    private bool hasPlayed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasPlayed && other.CompareTag("Player"))
+        {
+            audioSource.Play();
+            hasPlayed = true;
+        }
+    }
+
+}
--- The Darkness/Assets/Scripts/UIAppear.cs
+++ The Darkness/Assets/Scripts/UIAppear.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class UIAppear : MonoBehaviour
 {
     [SerializeField] private Image customImage;
+    
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             customImage.enabled = true;
+            
         }
     }
 
