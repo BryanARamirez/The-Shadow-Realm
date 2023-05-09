ebd47ea841075e9ab92ac0747b07ecf23abb1eab
Domags99 <Djaguilar@mail.csuchico.edu>
2023-05-09 09:42:14 -0700

    lighting fix
    lighting fix

--- The Darkness/Assets/Scripts/flickeringLights.cs
+++ The Darkness/Assets/Scripts/flickeringLights.cs
@@ -18,10 +18,10 @@ public class flickeringLights : MonoBehaviour
     {
         isFlickering = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.01f, 0.2f);
+        timeDelay = Random.Range(0.01f, 0.9f);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.01f, 0.2f);
+        timeDelay = Random.Range(0.01f, 0.9f);
         yield return new WaitForSeconds(timeDelay);
         isFlickering = false;
 
